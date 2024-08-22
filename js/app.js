e9ded280cef537e51d2d974bf578f478320936ea
sina <anonymous8884person@gmail.com>
2024-08-22 15:34:47 +0330

    js file started

---
+++ js/app.js
@@ -0,0 +1,56 @@
+const $ = document
+const searchBtn = $.querySelector(".search-btn");
+const darkModeBtn = $.querySelector(".darkmode-btn");
+const ChevronDownBtn = $.querySelector(".chevrondown-btn");
+const backSearchModal = $.querySelector(".back-search-modal");
+const backSigninModal = $.querySelector(".back-modal");
+const blogPostsWrapper = $.querySelector(".blog-posts-wrapper");
+const profileInformationsSection = $.querySelector(".profile-informations-section");
+
+
+
+// serach modal started
+function openSearchModal() {
+    backSearchModal.style.display = "block"
+    $.body.style.overflow = "hidden"
+    $.body.classList.add("pad-t-15");
+}
+
+function closeSearchModal() {
+    backSearchModal.style.display = "none"
+    $.body.style.overflow = "auto"
+    $.body.classList.remove("pad-t-15");
+}
+
+searchBtn.addEventListener("click", openSearchModal);
+backSearchModal.addEventListener("click", event => {
+    if(event.target === backSearchModal){
+        closeSearchModal()
+    }
+})
+// search modal ended
+
+// darkmode started
+function darkMode(){
+    blogPostsWrapper.style.backgroundColor = "#020617"
+}
+darkModeBtn.addEventListener("click", darkMode)
+// sign in modal started
+function openSigninModal() {
+    backSigninModal.style.display = "block"
+    $.body.style.overflow = "hidden"
+    $.body.classList.add("pad-t-15");
+}
+
+function closeSigninModal() {
+    backSigninModal.style.display = "none"
+    $.body.style.overflow = "auto"
+    $.body.classList.remove("pad-t-15");
+}
+ChevronDownBtn.addEventListener("click", openSigninModal)
+backSigninModal.addEventListener("click", event => {
+    if(event.target === backSigninModal){
+        closeSigninModal()
+    }
+})
+// sign in modal ended
