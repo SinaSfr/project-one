bdf4151cf3d706e6ec024bacbf36ec5dc336b637
sina <anonymous8884person@gmail.com>
2024-08-24 00:06:39 +0330

    page2 start to coding

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 const $ = document
+const header = $.querySelector("header")
+const headerPage2 = $.querySelector(".header-page2");
 const searchBtn = $.querySelector(".search-btn");
 const darkModeBtn = $.querySelector(".darkmode-btn");
 const ChevronDownBtn = $.querySelector(".chevrondown-btn");
@@ -6,12 +8,15 @@ const backSearchModal = $.querySelector(".back-search-modal");
 const backSigninModal = $.querySelector(".back-modal");
 const questionIcon = $.querySelector(".question-icon");
 const pencilIcon = $.querySelector(".pencil-icon");
+const chevronLeftBtn = $.querySelector(".chevronleft-btn");
+const bars3Btn = $.querySelector(".bars3-btn");
 const blogPostsWrapper = $.querySelector(".blog-posts-wrapper");
 const profileInformationsSection = $.querySelector(".profile-informations-section");
 
 
 
-// serach modal started
+if(header){
+    // serach modal started
 function openSearchModal() {
     backSearchModal.style.display = "block"
     $.body.style.overflow = "hidden"
@@ -36,11 +41,11 @@ backSearchModal.addEventListener("click", event => {
 function darkMode(){
    $.body.classList.toggle("darkmode")
    if($.body.classList.contains("darkmode")){
-       darkModeBtn.firstChild.src = "icons/sun.svg"
+       darkModeBtn.firstChild.src = "../icons/sun.svg"
        questionIcon.setAttribute("stroke", "#f8fafc");
        pencilIcon.setAttribute("stroke", "#f8fafc");
     } else{
-        darkModeBtn.firstChild.src = "icons/moon.svg"
+        darkModeBtn.firstChild.src = "../icons/moon.svg"
         questionIcon.setAttribute("stroke", "#0000004d");
         pencilIcon.setAttribute("stroke", "#0000004d");
     }
@@ -67,3 +72,33 @@ backSigninModal.addEventListener("click", event => {
     }
 })
 // sign in modal ended
+}
+
+// sticky header
+let lastScrollTop = 0
+window.addEventListener("scroll", () => {
+    let currentScrollTop = window.pageYOffset || $.documentElement.scrollTop;
+    if(currentScrollTop > lastScrollTop){
+        headerPage2.style.top= "-10rem"
+    } else{
+        headerPage2.style.top = "0"
+    }
+    lastScrollTop = currentScrollTop
+})
+// end sticky header
+
+// chevronleft evented to homepage
+chevronLeftBtn.addEventListener("click", () => {
+    window.location.href = "/"
+})
+// end of chevronleft evented to homepage
+
+
+// fetch footer for everypage
+fetch("../html/footer.html")
+    .then(response => response.text())
+    .then(data => {
+        $.getElementById("footer-place").innerHTML = data;
+    })
+    .catch(err => console.error(err))
+    // end fetch footer
